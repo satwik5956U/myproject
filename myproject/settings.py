5cc35ab2a4a9e930a32de7f1e0d77b05828d0982
satwik5956U <vvit20bq1a05g8@gmail.com>
2023-06-14 13:48:39 +0530

    added 2

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangopro',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-        'USER':'root',
-        'PASSWORD':'Satwik@2003',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
